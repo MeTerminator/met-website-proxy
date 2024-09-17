fdc2aeada03a45c5e43f85d423aa7ff396bc5ff6
MeTerminator <42143735+MeTerminator@users.noreply.github.com>
2024-09-17 14:10:19 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,7 @@
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
-
-@app.route('/about')
-def about():
-    return 'About'
+    return redirect('https://www.met6.top:444/', code=301)
