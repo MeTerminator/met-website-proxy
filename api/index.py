a080e609db56c12d4c2a01751f90e0c075fe5fdc
MeTerminator <42143735+MeTerminator@users.noreply.github.com>
2024-10-02 00:33:01 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,6 +2,15 @@ from flask import Flask, redirect
 
 app = Flask(__name__)
 
+MET_WEBSITE_URL = 'https://www.met6.top:444/'
+
 @app.route('/')
 def home():
-    return redirect('https://www.met6.top:444/', code=301)
+    return redirect(MET_WEBSITE_URL, code=301)
+
+@app.errorhandler(404)
+def not_found(e):
+    return redirect(MET_WEBSITE_URL, code=301)
+
+if __name__ == '__main__':
+    app.run()
